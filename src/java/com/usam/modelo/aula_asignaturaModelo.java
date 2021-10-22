955260d62d831f92131292523abb71c74a9188f6
Oscar Lopez <oelsuarez91@gmail.com>
2021-10-21 21:18:42 -0600

    Subiendo Materialize

---
+++ src/java/com/usam/modelo/aula_asignaturaModelo.java
@@ -0,0 +1,12 @@
+package com.usam.modelo;
+
+public class aula_asignaturaModelo {
+
+    int idAula_Asignatura;
+    int idaula;
+    int asignatura;
+    int estado;
+    int dia;
+    int hora;
+
+}
