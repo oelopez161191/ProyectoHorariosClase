00f7fa64e925e219ae933fdef299389d66b157a9
Oscar Lopez <oelsuarez91@gmail.com>
2021-10-21 22:20:28 -0600

    DAO Docente

--- src/java/com/usam/modelo/aula_asignaturaModelo.java
+++ src/java/com/usam/modelo/aula_asignaturaModelo.java
@@ -9,4 +9,54 @@ public class aula_asignaturaModelo {
     int dia;
     int hora;
 
+    public int getIdAula_Asignatura() {
+        return idAula_Asignatura;
+    }
+
+    public void setIdAula_Asignatura(int idAula_Asignatura) {
+        this.idAula_Asignatura = idAula_Asignatura;
+    }
+
+    public int getIdaula() {
+        return idaula;
+    }
+
+    public void setIdaula(int idaula) {
+        this.idaula = idaula;
+    }
+
+    public int getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(int asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    
+    
 }
---
+++ src/java/com/usam/modelo/docenteModelo.java
@@ -0,0 +1,108 @@
+package com.usam.modelo;
+
+public class docenteModelo {
+    
+    int iddocente;
+    String codigo_interno;
+    String nombre;
+    String segundo_nombre;
+    String tercer_nombre;
+    String apellido;
+    String segundo_apellido;
+    String direccion;
+    String email;
+    String dui;
+    String isss;
+
+    public int getIddocente() {
+        return iddocente;
+    }
+
+    public void setIddocente(int iddocente) {
+        this.iddocente = iddocente;
+    }
+
+    public String getCodigo_interno() {
+        return codigo_interno;
+    }
+
+    public void setCodigo_interno(String codigo_interno) {
+        this.codigo_interno = codigo_interno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSegundo_nombre() {
+        return segundo_nombre;
+    }
+
+    public void setSegundo_nombre(String segundo_nombre) {
+        this.segundo_nombre = segundo_nombre;
+    }
+
+    public String getTercer_nombre() {
+        return tercer_nombre;
+    }
+
+    public void setTercer_nombre(String tercer_nombre) {
+        this.tercer_nombre = tercer_nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSegundo_apellido() {
+        return segundo_apellido;
+    }
+
+    public void setSegundo_apellido(String segundo_apellido) {
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String dirección) {
+        this.direccion = dirección;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getIsss() {
+        return isss;
+    }
+
+    public void setIsss(String isss) {
+        this.isss = isss;
+    }
+    
+    
+    
+    
+}
