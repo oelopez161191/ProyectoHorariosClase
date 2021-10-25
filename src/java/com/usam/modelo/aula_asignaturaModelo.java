99f31bca599b464775a3f75880f94659013921fa
oelopez161191 <66098086+oelopez161191@users.noreply.github.com>
2021-10-25 01:22:47 -0600

    Add files via upload

---
+++ src/java/com/usam/modelo/aula_asignaturaModelo.java
@@ -0,0 +1,62 @@
+package com.usam.modelo;
+
+public class aula_asignaturaModelo {
+
+    int idAula_Asignatura;
+    int idaula;
+    int asignatura;
+    int estado;
+    int dia;
+    int hora;
+
+    public int getIdAula_Asignatura() {
+        return idAula_Asignatura;
+    }
+
+    public void setIdAula_Asignatura(int idAula_Asignatura) {
+        this.idAula_Asignatura = idAula_Asignatura;
+    }
+
+    public int getIdaula() {
+        return idaula;
+    }
+
+    public void setIdaula(int idaula) {
+        this.idaula = idaula;
+    }
+
+    public int getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(int asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    
+    
+}
